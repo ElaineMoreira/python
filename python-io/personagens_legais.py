46f2c40a8ebcd25fa6347666778a91de1e6eb0bc
Elaine <elainefelipemoreira@gmail.com>
2022-05-30 10:15:23 -0300

    feat: 🎸 add for in for read file json

--- python-io/personagens_legais.py
+++ python-io/personagens_legais.py
@@ -1,13 +1,22 @@
-characters_file = open("meus-persongens-favoritos.txt", mode="w")
+# characters_file = open("meus-persongens-favoritos.txt", mode="w")
 
-characters_file.write("Tio Patinhas\n")
-characters_file.write("Neo\n")
-characters_file.write("Homem Aranha\n")
+# characters_file.write("Tio Patinhas\n")
+# characters_file.write("Neo\n")
+# characters_file.write("Homem Aranha\n")
 
-print("Batman", file=characters_file)
+# print("Batman", file=characters_file)
 
-more_characters = ["Hulk\n", "Thor\n"]
+# more_characters = ["Hulk\n", "Thor\n"]
 
-characters_file.writelines(more_characters)
+# characters_file.writelines(more_characters)
 
-characters_file.close()
+# characters_file.close()
+
+############ JSON ############
+
+# import json
+
+# with open("superheroes.json") as superheroes_file:
+#   superheroes_list = json.load(superheroes_file)
+#   for superheroe in superheroes_list:
+#     print(superheroe["after_ego"])
