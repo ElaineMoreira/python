a484a0ea8a04e2ac40fcd64fe4119f8764d26f9e
Elaine <elainefelipemoreira@gmail.com>
2022-05-30 09:46:24 -0300

    feat: 🎸 create config ambient python

---
+++ python-io/personagens_legais.py
@@ -0,0 +1,13 @@
+characters_file = open("meus-persongens-favoritos.txt", mode="w")
+
+characters_file.write("Tio Patinhas\n")
+characters_file.write("Neo\n")
+characters_file.write("Homem Aranha\n")
+
+print("Batman", file=characters_file)
+
+more_characters = ["Hulk\n", "Thor\n"]
+
+characters_file.writelines(more_characters)
+
+characters_file.close()
